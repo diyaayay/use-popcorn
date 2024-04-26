f65d901dbced73ffccf397f8470c66adb124fcdc
Diya <diya.solanki.31@gmail.com>
2024-04-26 14:00:29 +0530

    watched movies displays pre rated rating

--- src/App-v1.js
+++ src/App-v1.js
@@ -134,6 +134,7 @@ const average = (arr) =>
       <Box>{
         selectedId?<MovieDetails 
         selectedId={selectedId} 
+        watched={watched}
         onCloseMovie={handleCloseMovie}
         onAddWatched={handleAddWatched}
         />: <>
@@ -253,10 +254,12 @@ function Movie({movie,onSelectMovie}){
 // </div>
 // }
 
-function MovieDetails({selectedId,onCloseMovie,onAddWatched}){
+function MovieDetails({selectedId,onCloseMovie,onAddWatched,watched}){
 const [movie,setMovie]=useState({});
 const[isLoading,setIsLoading]=useState(false);
 const [userRating,setUserRating]=useState('');
+const isWatched=watched.map(movie=>movie.imdbID).includes(selectedId);
+const watchedUserRating=watched.find(movie=>movie.imdbID===selectedId)?.userRating;
 
 const {
   Title:title,
@@ -312,12 +315,15 @@ useEffect(function(){
   </div>
   </header>
   <section>
-  <div className="rating"><StarRating maxRating={10}
+    <div className="rating">
+    {!isWatched?<><StarRating maxRating={10}
    size={24} onSetRating={setUserRating}/>
- { userRating>0 &&<button className="btn-add" 
+ { userRating>0 && <button className="btn-add" 
   onClick={handleAddWatched}>
   + Add to list
-  </button>}
+  </button>}</> :
+  <p>You rated with movie {watchedUserRating}</p>
+  }
   </div>
   
     <p><em>{plot}</em></p>
