d0aed1cc13db37cd94214e9df5c685bf33e47b34
Diya <diya.solanki.31@gmail.com>
2024-04-28 19:12:45 +0530

    removes useEffects multiple renders

--- src/App-v1.js
+++ src/App-v1.js
@@ -1,54 +1,6 @@
  import { useState, useEffect } from "react";
  import StarRating from "./StarRating";
 
-const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster: 
-      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
-];
-
-const tempWatchedData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: "tt0088763",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
-];
-
-
 const Api_Key="e2167ce1";
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
@@ -83,15 +35,18 @@ const average = (arr) =>
           setWatched(watched=>watched.filter((movie)=>movie.imdbID!==id));
         }
 
-
-    
     useEffect(function() {
+
+      
+        const controller = new AbortController();
+      
       
       async function fetchMovies(){
         try {setIsLoading(true);
           setError("");
         const res=await fetch(`http://www.omdbapi.com/?apikey=${Api_Key}
-      &s=${query}`);
+      &s=${query}`, {signal: controller.signal}
+    );
       
 
       if(!res.ok){
@@ -103,9 +58,11 @@ const average = (arr) =>
         throw new Error("Movie not found.");  
        }
        setMovies(data.Search);
+       setError("");
       }catch(err){
         console.error(err.message);
-        setError(err.message);
+        if(err.name!=="AbortError"){
+        setError(err.message);}
             }finally{
               setIsLoading(false);
             } 
@@ -114,18 +71,21 @@ const average = (arr) =>
             setMovies([]);
             setError("");
             return ;
-          }
+          }  
       fetchMovies();
+      return function(){
+        controller.abort();
+      };
 
     },[query]);
     
-   
+
   
     return (
       <>
   <NavBar>
   <Search query={query} setQuery={setQuery}/>
-  <Numresults movies={movies} />
+  <NumResults movies={movies} />
   </NavBar>
   <Main>
       <Box>
@@ -181,9 +141,9 @@ const average = (arr) =>
   />
   }
 
-  function Numresults({movies}){
+  function NumResults({movies}){
     <p className="num-results">
-    Found <strong>{movies.length}</strong> results
+    Found <strong>{movies.length}</strong> top results
   </p>
   }
 
@@ -304,6 +264,30 @@ useEffect(function(){
      }
   getMovieDetails();
 },[selectedId]);
+
+useEffect(function()
+{function callBack(e){
+  if(e.code==="Escape"){
+   onCloseMovie();
+  }
+}
+  document.addEventListener('keydown',callBack);
+
+  return function(){
+    document.removeEventListener('keydown',callBack );
+  }
+},[onCloseMovie]);
+
+useEffect(function(){
+  if(!title)
+  return;
+  document.title=`Movie | ${title}`;
+
+  return function() {
+    document.title='usePopcorn';
+  }
+ },[title]
+);
 
   return <div className="details">
   {isLoading?<Loader/>:<>
